dfa3ad41734f9a653e99665f656a5c472a0d5a9a
unicornstrux <cpatmont@gmail.com>
2026-03-19 14:17:47 -0700

    chore: remove dead JS blocks and unused commented-out HTML sections

--- js/main.js
+++ js/main.js
@@ -63,15 +63,6 @@ $(window).on('load', function() {
 
 
 	/*------------------
-  		YOUTUBE BG
-  	--------------------*/
-	$('#video-bg').YTPlayer({
-		fitToBackground: true,
-		videoId: 'YJNdx5Ses7s'//Set Your Youtube Video ID
-	});
-
-
-	/*------------------
   		MOUSE PARALLAX
   	--------------------*/
 	$('#background-1').mouseParallax({
@@ -80,105 +71,8 @@ $(window).on('load', function() {
 
 
 	/*------------------
-  		PROGRESS BAR
-  	--------------------*/
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
-		}
-	});
-
-
-
-
-	/*------------------
-  		REVIEW CAROUSEL
-  	--------------------*/
-	$('#review-carousel').owlCarousel({
-		dots: false,
-		nav: true,
-		loop: true,
-		margin:30,
-		smartSpeed: 700,
-		items:1,
-		autoplay:true,
-		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
-	});
-
-
-	/*------------------
-		MAGNIDIC POPUP
-	--------------------*/
-	$('.default-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 400,
-		zoom:{enabled: true, duration: 300}
-	});
-
-	$('.content-popup').magnificPopup({
-		type: 'inline',
-		removalDelay: 400
-	});
-
-	$('.video-popup').magnificPopup({
-		type: 'iframe',
-		autoplay : true
-	});
-
-
-	/*------------------
 		WOW JS
 	--------------------*/
 	new WOW().init();
-
-
-	/*------------------
-		CONTACT FORM
-	--------------------*/
-	$('#contact-form').on('submit', function() {
-		var send_btn = $('#send-form'),
-			form = $(this),
-			formdata = $(this).serialize(),
-			chack = $('#form-chack');
-			send_btn.text('Wait...');
-
-		function reset_form(){
-		 	$("#name").val('');
-			$("#email").val('');
-			$("#massage").val('');
-		}
-
-		$.ajax({
-			url:  $(form).attr('action'),
-			type: 'POST',
-			data: formdata,
-			success : function(text){
-				if (text == "success"){
-					send_btn.addClass('done');
-					send_btn.text('Success');
-					setTimeout(function() {
-						reset_form();
-						send_btn.removeClass('done');
-						send_btn.text('Send Massage');
-					}, 3000);
-				}
-				else {
-					reset_form();
-					send_btn.addClass('error');
-					send_btn.text('Error');
-					setTimeout(function() {
-						send_btn.removeClass('error');
-						send_btn.text('Send Massage');
-					}, 5000);
-				}
-			}
-		});
-		return false;
-	});
 
 })(jQuery);
